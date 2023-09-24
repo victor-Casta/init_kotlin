678e38f3cc2c80c972271de8e6b5c47c418d6f8d
Victor Casta <victorarroyavecastaneda@gmail.com>
2023-09-24 17:30:25 -0500

    adapted results

--- app/src/main/java/castadev/online/calculatorinit/index/ResultActivity.kt
+++ app/src/main/java/castadev/online/calculatorinit/index/ResultActivity.kt
@@ -9,27 +9,6 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-<<<<<<< HEAD
-        val addResult = intent.getStringExtra("sum")
-        val subtractionResult = intent.getStringExtra("subtraction")
-        val multiplicationResult = intent.getStringExtra("multiplication")
-        val divisionResult = intent.getStringExtra("division")
-
-        val resultText = findViewById<TextView>(R.id.result_text)
-        resultText.text = addResult
-
-        val subtractionView = findViewById<TextView>(R.id.subtraction_result)
-        subtractionView.text = subtractionResult
-
-        val multiplicationView = findViewById<TextView>(R.id.multiplication_result)
-        multiplicationView.text = multiplicationResult
-
-        val divisionView = findViewById<TextView>(R.id.division_result)
-        divisionView.text = divisionResult
-
-    }
-}
-=======
 
         val addResult = intent.getIntExtra("sum", 0).toString()
         val subtractionResult = intent.getIntExtra("difference", 0).toString()
@@ -49,4 +28,3 @@ class ResultActivity : AppCompatActivity() {
         divisionView.text = "Quotient: $divisionResult"
     }
 }
->>>>>>> 95ad840 (adapted results)
